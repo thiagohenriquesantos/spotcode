aff4d7b27557791bcdfde4f0bc6d920654d1ef76
thiagohenriquesantos <thiagosantosengcivil@gmail.com>
2020-04-16 16:57:01 -0300

    backend done

--- config/routes.rb
+++ config/routes.rb
@@ -2,21 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
   root to: "home#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  namespace :api, defaults: { format: :json } do
-    namespace :v1 do
-      resources :dashboard, only: :index
-      resources :search, only: :index
-      resources :favorites, only: :index
-      resources :categories, only: %i[index show]
-      resources :albums, only: :show do
-      resources :recently_heards, only: :create
-      end
-      resources :songs, only: [] do
-        concerns :favoritable, favoritable_type: 'Song'
-      end 
-    end
-  end
 
   concern :favoritable do |options|
     shallow do
@@ -24,4 +9,19 @@ Rails.application.routes.draw do
       delete "/favorite", { to: "favorites#destroy", on: :member }.merge(options)
     end
   end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :dashboard, only: :index
+      resources :search, only: :index
+      resources :favorites, only: :index
+      resources :categories, only: %i[index show]
+      resources :favorites, only: :index
+      resources :albums, only: :show do
+        resources :recently_heards, only: :create
+      end
+    end
+  end
+
+
 end
