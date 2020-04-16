ba39b44156166a5e075f2035a1c945bc86403a3a
thiagohenriquesantos <thiagosantosengcivil@gmail.com>
2020-04-16 13:16:48 -0300

    endpoint discovery created

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   get 'home/index'
   root to: "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :dashboard, only: :index
+    end
+  end
 end
