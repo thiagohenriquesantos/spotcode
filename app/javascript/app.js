0731ee1c3e00eac16bd348a9c6e35f2707b4b18f
thiagohenriquesantos <thiagosantosengcivil@gmail.com>
2020-04-23 12:01:02 -0300

    navbar created

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -3,11 +3,13 @@ import "react-bulma-components/dist/react-bulma-components.min.css";
 import Routes from "./routes";
 import "app.scss";
 import { BrowserRouter } from "react-router-dom";
+import Menu from "./components/common/menu";
 
 const App = () => {
   return (
     <Fragment>
       <BrowserRouter>
+        <Menu />
         <Routes />
       </BrowserRouter>
     </Fragment>
